24a0680ed52524e4aa3b90310e687458e2a0685f
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-05-16 09:38:54 +0200

    commit

--- ex 5 zest 4/Program.cs
+++ ex 5 zest 4/Program.cs
@@ -10,6 +10,26 @@ namespace ex_5_zest_4
     {
         static void Main(string[] args)
         {
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine("Area of the square: " + rectangle.Area(5));
+            Console.WriteLine("Area of the rectangle: " + rectangle.Area(5, 2));
+            Console.ReadKey();
+        }
+    }
+    interface IFigure
+    {
+        int Area(int a);
+        int Area(int a, int b);
+    }
+    class Rectangle : IFigure
+    {
+        public int Area(int a)
+        {
+            return a * a;
+        }
+        public int Area(int a, int b)
+        {
+            return a * b;
         }
     }
 }
